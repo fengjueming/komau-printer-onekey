2345b79520825128d0250af1b2611eff35f64ee8
Jueming <me@ming.moe>
2019-05-18 07:54:57 +0900

    new script

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 1. Open Terminal
 2. Run
 	sudo bash
-3. Run
-	a
+3. Run (1mx0000x rewrite to your KOMAnetID)
+	bash <(curl -s https://raw.githubusercontent.com/fengjueming/komau-printer-onekey/master/print.sh) 1mx0000x
 4. Done
+
+- ドライバーと印刷システムクライアント両方ともインストールしてない時だけ動ける
+- 学校のサーバーが遅いため、ダウンロードがちょっと遅めになる
--- print.sh
+++ print.sh
@@ -3,21 +3,44 @@
 export PDD="/Library/Printers/PPDs/Contents/Resources/EPSON LP-S9000.gz"
 export SPSE="/Library/Printers/SPSE/SPSENotifier.app"
 
-[[ "$EUID" -ne '0' ]] && echo "Error:This script must be run as root!" && exit 1;
+function root_check()
+{
+if [[ $EUID -ne 0 ]]; then
+  echo "管理員権限がありません、sudo bashしてからもう一回実行してください" 1>&2
+  exit 1
+fi
+}
 
+function INSDEV()
+{
+    curl https://www.komazawa-u.ac.jp/~joho/printer2015/Komazawa_u_pd_Mac_Ver4.zip -o /tmp/Komazawa_u_pd_Mac_Ver4.zip
+    #curl -O https://ming.moe/Komazawa_u_pd_Mac_Ver4.zip
+    unzip -o Komazawa_u_pd_Mac_Ver4.zip -x '__MACOSX/*'
+    installer -pkg 駒澤大学_プリンタードライバー_forMac_Ver4/駒澤大学_プリンタードライバーforMac.pkg -target /
+    installer -pkg 駒澤大学_プリンタードライバー_forMac_Ver4/SPSE\ CUPS\ Driver\ Input\ User\ Installer.pkg -target /
+    rm -rf 駒澤大学_プリンタードライバー_forMac_Ver4
+    rm -rf Komazawa_u_pd_Mac_Ver4.zip
+    echo "ドライバーインストール完了"
+}
 
+function Install()
+{
 if [ ! -d "$SPSE" ]; then
+    if [ ! -f "$PDD" ]; then
+        echo "SPSEとプリンタードライバーが見つかりませんでした"
+        echo "これからドライバーをダウンロードします"
+        INSDEV
+    else
     echo "Error:Please install SPSE."
-    exit 1
+    exit
+    fi
+else
+if [ ! -r "$PDD" ]; then
+echo "Error:Please install Printer Devicers."
+exit
 fi
-echo "SPSE OK"
-
-if [ ! -f "$PDD" ]; then
-    echo "Error:Please install Printer Devicers."
-    exit 1
+echo "SPSE&Drivers OK"
 fi
-echo "Devicers OK"
-
 
 lpadmin -p Mono -E -v spssock://10.1.1.201:60000/Mono -m '/Library/Printers/PPDs/Contents/Resources/EPSON LP-S3550.gz'
 lpadmin -p Color -E -v spssock://10.1.1.201:60000/Color -m 'Library/Printers/PPDs/Contents/Resources/EPSON LP-S9000.gz'
@@ -25,7 +48,19 @@ echo "プリンター追加完了"
 echo "現在システムに設定したプリンター："
 lpstat -s
 echo "以上です"
+echo "SPSEUserに書き込みします"
+mkdir -p /Users/Shared/SPSE/
+touch /Users/Shared/SPSE/SPSEUser
+echo -n $ID > /Users/Shared/SPSE/SPSEUser
+echo "完了"
+}
 
-read -p "KOMAnetIDを入力してください: " KOMAID
-echo -n $KOMAID > /Users/Shared/SPSE/SPSEUser
-echo "OK"
+if [ $# == '1' ]; then
+    root_check
+    echo "KOMAnetIDが$1になります"
+    ID=$1
+    Install
+else
+    echo "最後にKOMAnetIDをつけてください"
+    echo "例：bash <(curl -s https://raw.githubusercontent.com/fengjueming/komau-printer-onekey/master/print.sh) 1mx0000x"
+fi
